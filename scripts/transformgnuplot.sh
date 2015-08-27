f494f4ce21b1f6e929a02ff1c0f14c07812144a4
Tomas Hlavacek <tmshlvck@gmail.com>
2015-08-27 16:44:41 +0200

    Remove eps generator that does not fit here

--- scripts/transformgnuplot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed -i 's/^set term pngcairo .*/set terminal postscript eps enhanced monochrome font \"Helvetica,10\"/' $1
-
-sed -i -r 's#^set output \".*/([^/]+)\.png\"#set output "\1.eps"#' $1
-
